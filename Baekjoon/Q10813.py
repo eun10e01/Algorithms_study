4ca3074c515b072fff1cffa869e649eaf282a72c
eun10e01 <eun10e01@gmail.com>
2025-07-07 23:20:47 +0900

    Add Q2435.py:incomplete and week1.txt:incomplete about Q2435

--- Baekjoon/Q10813.py
+++ Baekjoon/Q10813.py
@@ -1,5 +1,6 @@
+# 간단한 swap 문제
 N, M = map(int, input().split(" "))
-ball = []
+ball = [] # 리스트 생성
 
 for n in range(N):
     ball.append(n + 1)
@@ -7,6 +8,7 @@ for n in range(N):
 for n in range(M):
     i, j = map(int, input().split(" "))
 
+    # 공 교환
     temp = ball[i - 1]
     ball[i - 1] = ball[j - 1]
     ball[j - 1] = temp
