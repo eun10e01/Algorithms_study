67d9522938afc94d5a5952ddc799c54ef6d0c958
eun10e01 <eun10e01@gmail.com>
2025-07-12 12:53:10 +0900

    Update Q2435.py:runtime error

--- Baekjoon/Q2435.py
+++ Baekjoon/Q2435.py
@@ -1,16 +1,15 @@
 N, K = map(int, input().split(" "))
-temperature = input().split(" ")
+temperature = list(map(int, input().split(" ")))
 
 sum = 0
 
-count = 0
 max = -1000000000
 
 for i in range(N - K + 1):
     sum = 0
     for j in range(K):
-        sum += int(temperature[j + count]) # j + count로 하면 다음 번에는 temperature[1]부터 더할 수 있음
-    count += 1
+        sum += int(temperature[j + i]) # j + i로 하면 다음 번에는 temperature[1]부터 더할 수 있음
+
     if(sum > max):
         max = sum
 
