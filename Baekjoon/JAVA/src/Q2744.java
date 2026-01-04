f54d8dc3b385f2eeb907962ec9fb604697661cbc
eun10e01 <eun10e01@gmail.com>
2026-01-04 13:12:59 +0900

    Add Q2744.java

---
+++ Baekjoon/JAVA/src/Q2744.java
@@ -0,0 +1,34 @@
+//대소문자 바꾸기
+/*
+[문제]
+영어 소문자와 대문자로 이루어진 단어를 입력받은 뒤, 대문자는 소문자로, 소문자는 대문자로 바꾸어 출력하는 프로그램을 작성하시오.
+[입력]
+첫째 줄에 영어 소문자와 대문자로만 이루어진 단어가 주어진다. 단어의 길이는 최대 100이다.
+[출력]
+첫째 줄에 입력으로 주어진 단어에서 대문자는 소문자로, 소문자는 대문자로 바꾼 단어를 출력한다.
+*/
+import java.util.Scanner;
+
+public class Q2744
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+            {
+                System.out.print(Character.toString(str.charAt(i)).toLowerCase());
+            }
+            else
+            {
+                System.out.print(Character.toString(str.charAt(i)).toUpperCase());
+            }
+        }
+
+        sc.close();
+    }
+}
