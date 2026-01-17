eadf5b31cce307119167e1b29a0236a9c2afe729
eun10e01 <eun10e01@gmail.com>
2026-01-17 14:40:47 +0900

    [Gold III] Title: 선물 전달, Time: 192 ms, Memory: 25568 KB -BaekjoonHub

---
+++ 백준/Gold/1947. 선물 전달/선물 전달.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		long D[] = new long[1000001];
+		
+		D[1] = 0;
+		D[2] = 1;
+		
+		for(int i = 3; i <= n; i++)
+		{
+			D[i] = (i - 1) * (D[i - 1] + D[i - 2]) % 1000000000;
+		}
+		
+		System.out.println(D[n]);
+		
+		sc.close();
+	}
+}
