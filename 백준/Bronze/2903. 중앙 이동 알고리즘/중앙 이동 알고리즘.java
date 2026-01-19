44ea89ac8d338f3836295ed1896ea2a46b59cce6
eun10e01 <eun10e01@gmail.com>
2026-01-19 16:22:10 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Long ans = 2L;
+
+        for(int i = 0; i < N; i++)
+        {
+            Long temp = ans - 1;
+
+            ans += temp;
+        }
+
+        System.out.println(ans * ans);
+
+        sc.close();
+    }
+}
