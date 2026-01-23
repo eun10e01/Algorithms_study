103e35e4cfba48a2c276bc37e69b8453bb210feb
eun10e01 <eun10e01@gmail.com>
2026-01-23 21:31:46 +0900

    [Gold III] Title: LCA, Time: 1956 ms, Memory: 133496 KB -BaekjoonHub

---
+++ 백준/Gold/11437. LCA/LCA.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main
+{
+	static ArrayList<Integer> tree[];
+	static int depth[];
+	static int parent[];
+	static boolean visited[];
+	
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		
+		tree = new ArrayList[N + 1];
+		
+		for(int i = 1; i <= N; i++)
+		{
+			tree[i] = new ArrayList<Integer>();
+		}
+		
+		for(int i = 0; i < N - 1; i++)
+		{
+			int s = scan.nextInt();
+			int e = scan.nextInt();
+			
+			tree[s].add(e);
+			tree[e].add(s);
+		}
+		
+		depth = new int[N + 1];
+		parent = new int[N + 1];
+		visited = new boolean[N + 1];
+		
+		BFS(1);
+		
+		int M = scan.nextInt();
+		
+		for(int i = 0; i < M; i++)
+		{
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			
+			int LCA = excuteLCA(a, b);
+			
+			System.out.println(LCA);
+		}
+	}
+	
+	static int excuteLCA(int a, int b)
+	{
+		if(depth[a] < depth[b])
+		{
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		
+		while(depth[a] != depth[b])
+		{
+			a = parent[a];
+		}
+		
+		while(a != b)
+		{
+			a = parent[a];
+			b = parent[b];
+		}
+		
+		return a;
+	}
+	
+	static void BFS(int node)
+	{
+		Queue<Integer> queue = new LinkedList<Integer>();
+		
+		queue.add(node);
+		visited[node] = true;
+		
+		int level = 1;
+		int size = 1;
+		int count = 0;
+		
+		while(!queue.isEmpty())
+		{
+			int now = queue.poll();
+			
+			for(int next : tree[now])
+			{
+				if(!visited[next])
+				{
+					visited[next] = true;
+					queue.add(next);
+					parent[next] = now;
+					depth[next] = level;
+				}
+			}
+			
+			count++;
+			
+			if(count == size)
+			{
+				count = 0;
+				size = queue.size();
+				level++;
+			}
+		}
+	}
+}
