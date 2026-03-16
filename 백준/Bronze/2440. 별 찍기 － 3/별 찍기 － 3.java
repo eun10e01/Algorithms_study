5e2ebb696464a1c6be538f6dba2436534f84b813
eun10e01 <eun10e01@gmail.com>
2026-03-16 22:20:22 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 172 ms, Memory: 14968 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = N - i; j >= 1; j--)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
