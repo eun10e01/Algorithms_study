013e99609bc482f3152a94289bf04457c4384f7b
eun10e01 <eun10e01@gmail.com>
2026-04-14 23:23:53 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << ".  .   ." << "\n";
+	cout << "|  | _ | _. _ ._ _  _" << "\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << "\n";
+
+	return 0;
+}
