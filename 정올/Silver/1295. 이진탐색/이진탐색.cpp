67bb8d1661f40fa6d34b093992cacfd0bc58eecc
eun10e01 <eun10e01@gmail.com>
2026-08-12 19:42:19 +0900

    Create 이진탐색.cpp

---
+++ 정올/Silver/1295. 이진탐색/이진탐색.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(const vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid + 1;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        int target;
+        cin >> target;
+
+        cout << binarySearch(arr, target) << '\n';
+    }
+
+    return 0;
+}
